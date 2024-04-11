335fa13c7e9d4dba464583b0682757dd21f58bc3
Naveenvenkatampalli <83108027+Naveenvenkatampalli@users.noreply.github.com>
2024-04-11 08:58:08 +0530

    Update test_meta.py

--- Tests/test_meta.py
+++ Tests/test_meta.py
@@ -1,1 +1,9 @@
-print("running metadata")
+from Tests.test_base import BaseTest
+import pytest
+
+@pytest.mark.run(order=2)#4
+@pytest.mark.usefixtures("drivers")
+class Test_SetMeta(BaseTest):
+    def test_set_meta(self, drivers, sanity_mode):
+        driver1,driver2,driver3,driver4=drivers
+        print("running metadata successfully")
